9a8236d82c1216bd3932cd1e776e248eed176a1e
Alaeddin Alzeybek <alaa@voltlines.com>
2021-10-06 16:18:24 +0300

    Updated no channel text and icon

--- Sources/Constant/SBUStringSet.swift
+++ Sources/Constant/SBUStringSet.swift
@@ -153,7 +153,7 @@ public class SBUStringSet: NSObject {
 
 
     // MARK: - Empty
-    public static var Empty_No_Channels = "Sohbetler Özelliğimiz Çok Yakında Sizlerle"
+    public static var Empty_No_Channels = "Çok Yakında..."
     public static var Empty_No_Messages = "Mesaj bulunmuyor"
     public static var Empty_No_Users = "Kullanıcı bulunmuyor"
     public static var Empty_No_Muted_Members = "Sessizde üye yok"
